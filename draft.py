344e7e4071a122f7cf7da938643d1747cd48b5f3
582217 <582217393@qq.com>
2019-10-15 18:13:56 +0800

    id info

--- draft.py
+++ draft.py
@@ -1,4 +1,7 @@
+import requests
 import os
+from lxml import etree
+import json
 
 
 def get_names_set(file_dir):
@@ -9,6 +12,54 @@ def get_names_set(file_dir):
     return l_fname
 
 
-s_names = get_names_set(r"D:\id")
+def get_proxy():
+    return requests.get("http://127.0.0.1:5010/get/").json()
 
-print(len(s_names))  
+
+def delete_proxy(proxy):
+    requests.get("http://127.0.0.1:5010/delete/?proxy={}".format(proxy))
+
+
+def get_page(url):
+    retry_count = 5
+    proxy = get_proxy().get("proxy")
+    while retry_count > 0:
+        try:
+            html = requests.get(url, proxies={
+                                "http": "http://{}".format(proxy)})
+            # 使用代理访问
+            return html
+        except Exception:
+            retry_count -= 1
+    delete_proxy(proxy)
+    return None
+
+
+s_names = get_names_set(r"D:\id")
+l_failnames = []
+d_res = {}
+
+while len(s_names):
+    print(len(s_names))
+    name = s_names.pop()
+    try:
+        search_url = 'https://datatracker.ietf.org/doc/search?name='+name + \
+            '&sort=&rfcs=on&activedrafts=on&olddrafts=on&by=group&group='
+        page = get_page(search_url)
+        tree = etree.HTML(page.text)
+        serch_res = tree.xpath(
+            '//*[@id="content"]/table/tbody[1]/tr/th[2]')[0].text
+        att = tree.xpath(
+            '//*[@id="content"]/table/tbody[2]/tr/td[2]/div/a')[0].attrib
+        doc_name = att['href'].split('/')[2]
+
+        print('serch res  '+serch_res)
+
+        json_url = 'https://datatracker.ietf.org'+att['href']+'doc.json'
+        jpage = get_page(json_url).text
+        data = json.loads(jpage)
+        h_count = len(data['rev_history'])
+        d_res[name] = [doc_name, serch_res.strip(), h_count]
+    except:
+        l_failnames.append(name)
+        print('fail  '+name)
